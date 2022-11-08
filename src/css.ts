85ddb2a4fe41ab4a88e7abf85afce7140fb3f37d
Anirudh Varma <anirudh@spotdraft.com>
2022-11-08 14:14:27 +0530

    add spacing scale and typography utils and variables (#1)

--- src/css.ts
+++ src/css.ts
@@ -2,6 +2,59 @@ import * as Tokens from "./tokens.json";
 import * as vscode from "vscode";
 
 const Z_INDEX_VALUES = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+const SPACING_SCALE_VALUES = {
+  s1: "0.3125rem",
+  s2: "0.625rem",
+  s3: "0.9375rem",
+  s4: "1.25rem",
+  s5: "1.875rem",
+  s6: "2.5rem",
+  l1: "3.125rem",
+  l2: "3.75rem",
+  l3: "5rem",
+  l4: "6.25rem",
+  l5: "7.5rem",
+};
+
+const TYPOGRAPHY_CLASSES = [
+  "sdk-text-body",
+  "sdk-text-body-light-2",
+  "sdk-text-body-light",
+  "sdk-text-body-medium",
+  "sdk-text-body-italic",
+  "sdk-text-body-italic-light",
+  "sdk-text-h1",
+  "sdk-text-h1-medium",
+  "sdk-text-h2",
+  "sdk-text-h2-medium",
+  "sdk-text-h3",
+  "sdk-text-h3-medium",
+  "sdk-text-h4",
+  "sdk-text-h4-medium",
+  "sdk-text-h5",
+  "sdk-text-h5-medium",
+  "sdk-text-h6",
+  "sdk-text-medium",
+  "sdk-text-medium-2",
+  "sdk-text-caption-1",
+  "sdk-text-caption-2",
+  "sdk-text-caption",
+  "sdk-text-caption-light",
+  "sdk-text-caption-medium",
+  "sdk-text-caption-italic",
+  "sdk-text-help",
+  "sdk-text-help-light",
+  "sdk-text-help-medium",
+  "sdk-text-help-italic",
+  "sdk-text-overline",
+  "sdk-text-overline-light",
+  "sdk-text-overline-medium",
+  "sdk-text-overline-italic",
+  "sdk-text-button",
+  "sdk-text-subtitle",
+  "sdk-text-error",
+  "sdk-text-sub-heading",
+];
 
 const VARIABLE_TRIGGER = "--";
 const HTML_CLASS_TRIGGER = "sdk-";
@@ -87,6 +140,105 @@ function zIndexAutoComplete(): vscode.CompletionItemProvider<vscode.CompletionIt
   };
 }
 
+function spacingUtilClassAutoComplete(): vscode.CompletionItemProvider<vscode.CompletionItem> {
+  return {
+    provideCompletionItems: function (
+      document: vscode.TextDocument,
+      position: vscode.Position
+    ) {
+      const entries = Object.entries(SPACING_SCALE_VALUES);
+      return entries
+        .map(([key, value]) => {
+          // for each item in the scale, the following
+          // classes are generated
+          return [
+            "p",
+            "px",
+            "py",
+            "pt",
+            "pl",
+            "pb",
+            "pr",
+            "m",
+            "mx",
+            "my",
+            "mt",
+            "ml",
+            "mb",
+            "mr",
+          ].map((className) => {
+            const item: vscode.CompletionItem = new vscode.CompletionItem(
+              `sdk-${className}-${key}`,
+              vscode.CompletionItemKind.Value
+            );
+            item.detail = value.toString();
+            return item;
+          });
+        })
+        .flat();
+    },
+  };
+}
+
+function typographyClassAutocomplete(): vscode.CompletionItemProvider<vscode.CompletionItem> {
+  return {
+    provideCompletionItems: function (
+      document: vscode.TextDocument,
+      position: vscode.Position
+    ) {
+      return TYPOGRAPHY_CLASSES.map((value, index) => {
+        const item: vscode.CompletionItem = new vscode.CompletionItem(
+          value,
+          vscode.CompletionItemKind.Value
+        );
+        return item;
+      });
+    },
+  };
+}
+
+function fontCSSPropsAutocomplete(): vscode.CompletionItemProvider<vscode.CompletionItem> {
+  return {
+    provideCompletionItems: function (
+      document: vscode.TextDocument,
+      position: vscode.Position
+    ) {
+      const items: vscode.CompletionItem[] = [];
+      items.push(
+        new vscode.CompletionItem(
+          "--sdk-font-color",
+          vscode.CompletionItemKind.Color
+        )
+      );
+      items.push(
+        new vscode.CompletionItem(
+          "--sdk-font-light-color",
+          vscode.CompletionItemKind.Color
+        )
+      );
+      items.push(
+        new vscode.CompletionItem(
+          "--sdk-font-weight-light",
+          vscode.CompletionItemKind.Value
+        )
+      );
+      items.push(
+        new vscode.CompletionItem(
+          "--sdk-font-weight-bold",
+          vscode.CompletionItemKind.Value
+        )
+      );
+      items.push(
+        new vscode.CompletionItem(
+          "--sdk-font-weight-default",
+          vscode.CompletionItemKind.Value
+        )
+      );
+      return items;
+    },
+  };
+}
+
 export function getCSSColorPropsAutocomplete() {
   return vscode.languages.registerCompletionItemProvider(
     "css",
@@ -136,6 +288,31 @@ export function getCssProviders() {
     vscode.languages.registerCompletionItemProvider(
       "html",
       zIndexAutoComplete(),
+      VARIABLE_TRIGGER
+    ),
+    vscode.languages.registerCompletionItemProvider(
+      "html",
+      spacingUtilClassAutoComplete(),
+      HTML_CLASS_TRIGGER
+    ),
+    vscode.languages.registerCompletionItemProvider(
+      "html",
+      typographyClassAutocomplete(),
+      HTML_CLASS_TRIGGER
+    ),
+    vscode.languages.registerCompletionItemProvider(
+      "css",
+      fontCSSPropsAutocomplete(),
+      VARIABLE_TRIGGER
+    ),
+    vscode.languages.registerCompletionItemProvider(
+      "scss",
+      fontCSSPropsAutocomplete(),
+      VARIABLE_TRIGGER
+    ),
+    vscode.languages.registerCompletionItemProvider(
+      "html",
+      fontCSSPropsAutocomplete(),
       VARIABLE_TRIGGER
     ),
   ];
